1bdf1b84a3693f2b5ff2a0b4c9d7a409f83e3a45
Adiv1978 <41622780+Adiv1978@users.noreply.github.com>
2026-06-21 23:20:52 -0400

    Agregar request para actualizar licencia

---
+++ Types/UpdateLicenciaRequest.cs
@@ -0,0 +1,12 @@
+﻿namespace ServPersonalCtr.Types
+{
+    public class UpdateLicenciaRequest
+    {
+        public int IdLicencia { get; set; }
+        public DateTime FecLicenciaIni { get; set; }
+        public DateTime FecLicenciaFin { get; set; }
+        public string Diagnostico { get; set; } = string.Empty;
+        public bool Auditoria { get; set; }
+        public string Observacion { get; set; } = string.Empty;
+    }
+}
